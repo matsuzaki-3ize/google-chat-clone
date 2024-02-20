8ffb2529d39936e55488d352c8ff03085002a2d2
Kensuke Matsuzaki <matsuzaki_kensuke@3-ize.jp>
2024-02-20 10:58:22 +0900

    Update to convert topic

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import { check_paths, get_user_info, get_groups_info, create_html } from './utils.js'
+import path from 'path'
+import minimist from 'minimist'
+
+import { check_paths, get_user_info, get_groups_info, create_html, create_html_index } from './utils.js'
 
 const main = () => {
     // Get the path to the Google Chat folder
-    const google_chat_path = process.argv.slice(2)[0]
+    const args = minimist(process.argv.slice(2))
+    const google_chat_path = args._[0]
+    let use_data_url = "use-data-url" in args && args["use-data-url"];
+    console.log("use_data_url", use_data_url)
 
     // Check if the path is valid and store the paths to the groups and users folders
-    const [groups_path, users_path] = check_paths(google_chat_path)
+    let [groups_path, users_path] = check_paths(google_chat_path)
+    console.log(groups_path, users_path)
 
     // Get user and group info
     const user_info = get_user_info(users_path)
@@ -14,11 +21,12 @@ const main = () => {
     // Create HTML files for each group
     const html_files = []
     for (let group_info of groups_info) {
-        html_files.push(create_html(group_info))
+        html_files.push(create_html(group_info, use_data_url))
     }
+    create_html_index(groups_path, groups_info)
 
     // Print the paths to the HTML files
-    console.log(html_files)
+    console.log("converted", html_files.length)
 }
 
 main()
--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import mime from 'mime'
 
 const check_users_path = (users_path) => {
     if (!fs.existsSync(users_path)) {
@@ -63,19 +64,30 @@ const get_group_info = (group_path, user_info) => {
 
     // Check if the file exists
     if (fs.existsSync(group_info_path)) {
-        let data = JSON.parse(fs.readFileSync(group_info_path))['members']
+        let data = JSON.parse(fs.readFileSync(group_info_path))
 
         // Remove the main user from the group info
-        data = data.filter(x => {
+        data.members = data.members.filter(x => {
             if (x['name'] !== user_info['name'] && x['email'] !== user_info['email']) {
                 return true
             }
         })
 
+        if (data.members.length == 0)
+            return undefined;
+
+        let name = data.name
+        if (!name) {
+            name = data.members.map(x => x['name']).join(", ")
+        }
+
         // Add the path property to the group info
-        data[0]['path'] = group_path
-        return data[0]
+        return {
+            'path': group_path,
+            'name': name,
+        }
     }
+    return undefined;
 }
 
 export const get_groups_info = (groups_path, user_info) => {
@@ -85,12 +97,32 @@ export const get_groups_info = (groups_path, user_info) => {
     for (let group of fs.readdirSync(groups_path)) {
         const current_group_path = path.join(groups_path, group)
 
-        if (fs.existsSync(path.join(current_group_path, 'messages.json'))) {
+        if (fs.existsSync(path.join(current_group_path, 'group_info.json'))) {
 
             // Get the group info and add it to the groups array
             const group_info = get_group_info(current_group_path, user_info)
             if (group_info !== undefined) {
-                groups.push(group_info)
+                group_info['path_name'] = group
+                group_info['type'] = "group"
+                const parent_group = JSON.stringify(group_info)
+                if (true || fs.existsSync(path.join(current_group_path, 'messages.json'))) {
+                    group_info['children'] = []
+                    groups.push(group_info)
+                    console.log("Group", current_group_path)
+                }
+                for (let topic of fs.readdirSync(current_group_path)) {
+                    let current_topic_path = path.join(current_group_path, topic)
+                    if (fs.existsSync(path.join(current_topic_path, 'messages.json'))) {
+                        console.log("Topic", current_topic_path)
+                        let topic_data = JSON.parse(parent_group)
+                        topic_data['type'] = "topic"
+                        topic_data['path'] = current_topic_path
+                        topic_data['path_name'] = topic
+                        topic_data['type'] = "topic"
+                        group_info['children'].push(topic_data)
+                        groups.push(topic_data)
+                    }
+                }
             }
         }
     }
@@ -108,7 +140,69 @@ const replace_bad_characters = (text) => {
     return text
 }
 
-export const create_html = (group_info) => {
+function path_name_to_id(path_name) {
+    let ids = path_name.split(' ')
+    return ids[ids.length-1]
+}
+
+export const create_html_index = (group_path, groups_info) => {
+    let html_path = path.join(group_path, 'index.html')
+    let list_messages = "<ul>"
+    for (let group of groups_info) {
+        if (group['type'] === "group") {
+            list_messages += `<li><a href="${path_name_to_id(group['path_name'])}.html">${replace_bad_characters(group['name'])}</a></li>\n`
+            // list_messages += create_message_html(topic)
+        }
+    }
+    list_messages += "</ul>"
+    fs.writeFileSync(html_path, create_html_file("Chat", list_messages))
+}
+
+export const create_html = (group_info, use_data_url) => {
+    let contents = ""
+    if (fs.existsSync(path.join(group_info['path'], 'messages.json'))) {
+        create_message_html(group_info, group_info['path'])
+    }
+
+    let dir_path = path.join(group_info['path'], '..')
+    let html_path = path.join(dir_path, path_name_to_id(group_info['path_name'])+'.html')
+
+    if (group_info["children"] && group_info["children"].length > 0) {
+        let list_messages = []
+        for (let topic of group_info["children"]) {
+            // list_messages += `<a href="${topic['topic']}/messages.html">${topic['name']}</a><br>`
+            list_messages.push(create_message_html(topic, dir_path, use_data_url))
+        }
+        // Sort by last message date
+        list_messages = list_messages.sort((a, b) => {
+            let aa = a['messages']
+            let bb = b['messages']
+            // FIXME Multi-language support
+            let da = aa[aa.length - 1]['created_date'].replace(/.曜日/g, '').replace(/[年月]/g, '-').replace(/[日]/g, '')
+            let db = bb[bb.length - 1]['created_date'].replace(/.曜日/g, '').replace(/[年月]/g, '-').replace(/[日]/g, '')
+            //console.log(da, db, Date.parse(da), Date.parse(db))
+            if (Date.parse(da) < Date.parse(db)) return -1;
+            return 1;
+        })
+        let contents = `<h1>${replace_bad_characters(group_info['name'])}</h1>\n`
+        for (const topic of list_messages) {
+            //contents += `<h2>${replace_bad_characters(topic.messages[0].creator.name)}</h2>\n`
+            contents += `<h2>${replace_bad_characters(topic.messages[0].created_date)}</h2>\n`
+            contents += topic['html']
+        }
+        list_messages.map(x => x['html']).join("\n")
+
+        // Create the html file
+        fs.writeFileSync(html_path, create_html_file(group_info['name'], contents))
+    }
+
+    return {
+        'name': group_info['name'],
+        'path': html_path
+    }
+}
+
+export const create_message_html = (group_info, dir_path, use_data_url) => {
     // Read the messages of the group
     const saved_messages = JSON.parse(fs.readFileSync(path.join(group_info['path'], 'messages.json'), { encoding: 'utf-8' }))['messages']
 
@@ -119,23 +213,26 @@ export const create_html = (group_info) => {
     // Add the div containing the messages
     let chat_messages = '<div class="row">'
     for (let message of saved_messages) {
-        const name = message['creator']['name']
-        const date = message['created_date']
-        const parts = date.split(',')
-        const day = parts[1]
-        const time = parts[2]
+        const name = (message['creator'] && message['creator']['name']) || "N/A";
+        const date = message['created_date'] || ""
+        //const parts = date.split(',')
+        //const day = parts[1]
+        //const time = parts[2]
 
         // Update html if the day has changed
+        /*
         if (day !== last_day) {
             let chat_message_day = `<div class="col-12"><h3 class="text-center my-4">${day}</h3></div>`
             chat_messages += chat_message_day
             last_day = day
         }
+        */
 
         // Change the name if it has changed
         if (name !== last_name) {
             chat_messages += '<div class="col-12"></div>'
-            let chat_message_title = `<div class="col-12 mt-3"><b>${name}</b> <span class="small">${time}</span></div>`
+            //let chat_message_title = `<div class="col-12 mt-3"><b>${name}</b> <span class="small">${time}</span></div>`
+            let chat_message_title = `<div class="col-12 mt-3"><b>${name}</b> <span class="small">${date}</span></div>`
             chat_messages += chat_message_title
             last_name = name
         }
@@ -152,29 +249,41 @@ export const create_html = (group_info) => {
         const attached_files = message['attached_files']
         if (attached_files) {
             const file = attached_files[0]['export_name']
-            let chat_message_file = process_attached_file(file, group_info)
+            let chat_message_file = process_attached_file(file, group_info, dir_path, use_data_url)
             chat_messages += chat_message_file
         }
+
+        chat_messages += '\n'
     }
     // Close the div containing the messages
     chat_messages += '</div>'
+    return {
+        "html": chat_messages,
+        "messages": saved_messages,
+    }
+}
 
+export const create_message_html_file = (group_info, override_file) => {
+    let dir_path = group_info['path']
+    let chat_messages = create_message_html(group_info, dir_path).html
     let number = 0
-    let html_path = path.join(group_info['path'], 'messages.html')
+    let html_path = path.join(dir_path, 'messages.html')
 
-    while (fs.existsSync(html_path)) {
-        // Get the name without the extension and the (number)
-        const html_name = html_path.split('.').slice(0, -1).join('.')
+    if (!override_file) {
+        while (fs.existsSync(html_path)) {
+            // Get the name without the extension and the (number)
+            const html_name = html_path.split('.').slice(0, -1).join('.')
 
-        // Check if the name has a number at the end
-        const match = html_name.match(/\((\d+)\)$/)
+            // Check if the name has a number at the end
+            const match = html_name.match(/\((\d+)\)$/)
 
-        // Update that number
-        if (match) {
-            number = parseInt(match[1])
-            html_path = html_name.replace(/\((\d+)\)$/, `(${++number})`) + '.html'
-        } else {
-            html_path = `${html_name} (${++number}).html`
+            // Update that number
+            if (match) {
+                number = parseInt(match[1])
+                html_path = html_name.replace(/\((\d+)\)$/, `(${++number})`) + '.html'
+            } else {
+                html_path = `${html_name} (${++number}).html`
+            }
         }
     }
 
@@ -188,7 +297,7 @@ export const create_html = (group_info) => {
 }
 
 let current_user = undefined
-const process_attached_file = (file, group_info) => {
+const process_attached_file = (file, group_info, dir_path, use_data_url) => {
     // Get name and extension of the file
     const extension = file.split('.').pop()
     let name = file.split('.').slice(0, -1).join('.')
@@ -203,22 +312,30 @@ const process_attached_file = (file, group_info) => {
     name = update_image_name(name)
 
     const file_path = path.join(group_info['path'], name + '.' + extension)
+    const rel_path = path.normalize(path.relative(dir_path, file_path)).split(path.sep).join('/')
 
     // Process each type of file
     if (is_image(file)) {
-        return `<div class="col-12"><img style="max-width: 40%; height:auto;" class="img-thumbnail" src="${file_path}"></div>`
+        if (!use_data_url || !fs.existsSync(file_path)) {
+            return `<div class="col-12"><img style="max-width: 40%; height:auto;" class="img-thumbnail" src="${rel_path}"></div>`
+        }
+        let base64 = fs.readFileSync(file_path, { encoding: 'base64' })
+        let mime_img = mime.getType(file_path)
+        console.log("Image", mime_img)
+        //return `<div class="col-12"><img style="max-width: 40%; height:auto;" class="img-thumbnail" src="${rel_path}"></div>`
+        return `<div class="col-12"><img style="max-width: 40%; height:auto;" class="img-thumbnail" src="data:i${mime_img};base64,${base64}"></div>`
     }
 
     if (is_video(file)) {
-        return `<div class="col-12"><video style="max-width: 40%; height:auto;" class="img-thumbnail" src="${file_path}" controls></video></div>`
+        return `<div class="col-12"><video style="max-width: 40%; height:auto;" class="img-thumbnail" src="${rel_path}" controls></video></div>`
     }
 
     if (is_audio(file)) {
-        return `<div class="col-12"><audio controls><source src="${file_path}">Your browser does not support the audio element.</audio></div>`
+        return `<div class="col-12"><audio controls><source src="${rel_path}">Your browser does not support the audio element.</audio></div>`
     }
 
     // Generic file
-    return `<div class="col-12"><a href="${file_path}">${file}</a></div>`
+    return `<div class="col-12"><a href="${rel_path}">${file}</a></div>`
 }
 
 let images = {}
